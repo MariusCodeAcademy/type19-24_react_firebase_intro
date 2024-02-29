49c5635d3c6443a5a6023adb5e3d6ad4a3be4105
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-29 09:51:22 +0200

    env works

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,14 +5,17 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIRE_B_apiKey,
-  authDomain: import.meta.env.FIRE_B_authDomain,
-  projectId: import.meta.env.FIRE_B_projectId,
-  storageBucket: import.meta.env.FIRE_B_storageBucket,
-  messagingSenderId: import.meta.env.FIRE_B_messagingSenderId,
-  appId: import.meta.env.FIRE_B_appId,
+  apiKey: import.meta.env.VITE_FIRE_B_apiKey,
+  authDomain: import.meta.env.VITE_FIRE_B_authDomain,
+  projectId: import.meta.env.VITE_FIRE_B_projectId,
+  storageBucket: import.meta.env.VITE_FIRE_B_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIRE_B_messagingSenderId,
+  appId: import.meta.env.VITE_FIRE_B_appId,
 };
 console.log('firebaseConfig ===', firebaseConfig);
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+
+export default app;
