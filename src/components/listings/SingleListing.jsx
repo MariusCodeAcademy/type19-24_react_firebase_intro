f6ff3318189a83f62b603a646c787dd5c16ab579
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-03-01 09:48:29 +0200

    photos

--- src/components/listings/SingleListing.jsx
+++ src/components/listings/SingleListing.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // atvaizduoti visa objeko info
 
 /*
-{
+const item = {
     id: 1,
     title: 'Ipad pro',
     body: 'Labai geras imkit',
@@ -16,12 +16,14 @@ import React from 'react';
 
 const SingleListing = ({ item }) => {
   return (
-    <li className='border p-5'>
-      <h3>{item.title}</h3>
-      <img src='' alt='title' />
-      <p>Price: </p>
-      <p>userId: </p>
-      <p>add id: </p>
+    <li className='border'>
+      <img className='h-56 object-cover' src={`/img/${item.imgUrl}`} alt={item.title} />
+      <div className='p-5'>
+        <h3 className='font-semibold text-lg'>{item.title}</h3>
+        <p>Price: ${item.price.toFixed(2)}</p>
+        <p>userId: {item.useriUid}</p>
+        <p>add id: {item.id}</p>
+      </div>
     </li>
   );
 };
